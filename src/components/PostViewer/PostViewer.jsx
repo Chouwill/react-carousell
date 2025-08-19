53206effb822d27bb9ef3b7d5c6822dac0f43f4c
Chouwill <will92068@gmail.com>
2025-08-19 16:25:35 +0800

    fix:修正文章輪播器

--- src/components/PostViewer/PostViewer.jsx
+++ src/components/PostViewer/PostViewer.jsx
@@ -3,6 +3,10 @@ import { getPlaceholder } from "../../api/method";
 import "./PostViewer.scss";
 
 export default function PostViewer() {
+  let data = document.querySelector(".main-Posts");
+  let nextPageBtn = document.querySelector(".nextPageBtn");
+  let returnPageBtn = document.querySelector(".returnPageBtn");
+  let page = document.querySelector(".page");
   let [postTexts, setPosts] = useState([]);
 
   let [startValue, setStart] = useState(0);
@@ -10,6 +14,8 @@ export default function PostViewer() {
   let [rangeValue, setRange] = useState(10);
 
   let [pageNum, setPages] = useState(1);
+
+  const [totalPage] = useState(10);
 
   let [mainStatus, setStatus] = useState(false);
 
@@ -23,21 +29,23 @@ export default function PostViewer() {
 
       setPosts((postTexts = res.data));
 
-      console.log("api回傳:", postTexts);
+      // console.log("api回傳:", postTexts);
     } catch (e) {
-      console.log(e);
+      // console.log(e);
     }
   };
 
   const showData = () => {
-    let data = document.querySelector(".main-Posts");
     console.log(data);
 
-
     console.log("data狀態", data);
-    
+    console.log("start", startValue); // start 值
+    console.log("range", rangeValue); // range 值
 
     data.style = "display:block";
+    nextPageBtn.style = "display:block";
+    returnPageBtn.style = "display:none";
+    page.style = "display:block";
   };
 
   useEffect(() => {
@@ -50,19 +58,19 @@ export default function PostViewer() {
     console.log("range", setRange(rangeValue + spacingNum)); // range 值
 
     console.log("00000", postTexts);
-    let nextPage = document.querySelector(".nextPage");
+    let nextPageBtn = document.querySelector(".nextPageBtn");
     // let showButton = document.querySelector(".showButton");
-    console.log(nextPage);
+    console.log(nextPageBtn);
 
     console.log("起始：", startValue, "終值：", rangeValue);
 
-    // setPosts(postTexts.slice(startValue, rangeValue));
-    if (pageNum === 10) {
+    if (startValue == 80) {
       setPages((pageNum = 10));
       setStart((startValue = 90));
       setRange((rangeValue = 100));
       console.log("沒有第0頁");
-      nextPage.style = "display:none";
+      nextPageBtn.style = "display:none";
+      returnPageBtn.style = "display:block";
     }
   };
 
@@ -76,12 +84,15 @@ export default function PostViewer() {
 
     console.log("起始：", startValue, "終值：", rangeValue);
 
-    if (pageNum === 1) {
+    if (rangeValue === 0) {
       setPages((pageNum = 1));
       setStart((startValue = 0));
       setRange((rangeValue = 10));
       console.log("沒有第0頁");
       returnPage.style = "display:none";
+      nextPageBtn.style = "display:none";
+    } else {
+      nextPageBtn.style = "display:block";
     }
   };
 
@@ -92,12 +103,14 @@ export default function PostViewer() {
         <button className="showButton" onClick={showData}>
           檢視資料筆數
         </button>
-        <h2>目前頁數{pageNum}/10</h2>
+        <h2 className="page">
+          目前頁數{pageNum}/{totalPage}
+        </h2>
         <div className="page-conrol">
-          <button className="nextPage" onClick={nextPage}>
+          <button className="nextPageBtn" onClick={nextPage}>
             下一頁
           </button>
-          <button className="returnPage" onClick={returnPage}>
+          <button className="returnPageBtn" onClick={returnPage}>
             上一頁
           </button>
         </div>
